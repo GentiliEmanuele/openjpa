02f0088aa20bc3a5c0d2e386d59566b982832d0d
GentiliEmanuele <egentili2000@gmail.com>
2026-01-28 21:44:15 +0100

    Implemented a test suite for the match methods

--- openjpa-lib/src/test/java/org/apache/openjpa/lib/util/SimpleRegexTest.java
+++ openjpa-lib/src/test/java/org/apache/openjpa/lib/util/SimpleRegexTest.java
@@ -15,34 +15,64 @@ public class SimpleRegexTest {
     private final String expr;
     private final boolean caseSensitive;
     private final Class<? extends Throwable> expectedException;
+    private final String target;
+    private final boolean expectedOutput;
 
     @Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-                {"", true, null},
-                {".", true, null},
-                {".*", true, null},
-                {"", false, null},
-                {".", false, null},
-                {".*", false, null},
-                {null, true, NullPointerException.class}
+                {"", true, null, "", true},
+                {"", true, null, "xa", false},
+                {"", true, null, "xabc", false},
+                {"x.", true, null, "", false},
+                {"x.", true, null, "xa", true},
+                {"x.", true, null, "xabc", false},
+                {"x.", true, null, "Xa", true},
+                {"x.*", true, null, "", false},
+                {"x.*", true, null, "xa", true},
+                {"x.*", true, null, "xabc", true},
+                {"x.*", true, null, "Xabc", true},
+                {"", false, null, "", true},
+                {"", false, null, "xa", false},
+                {"", false, null, "xabc", false},
+                {"x.", false, null, "", false},
+                {"x.", false, null, "xa", true},
+                {"x.", false, null, "xabc", false},
+                {"x.", false, null, "Xa", false},
+                {"x.*", false, null, "", false},
+                {"x.*", false, null, "xa", true},
+                {"x.*", false, null, "xabc", true},
+                {"x.*", false, null, "Xabc", false},
+                {null, false, NullPointerException.class, "Xabc", false},
         });
     }
 
-    public SimpleRegexTest(String expr, boolean caseSensitive, Class<? extends Throwable> expectedException) {
+    public SimpleRegexTest(String expr, boolean caseSensitive, Class<? extends Throwable> expectedException, String target, boolean expectedOutput) {
         this.expr = expr;
         this.caseSensitive = caseSensitive;
         this.expectedException = expectedException;
+        this.target = target;
+        this.expectedOutput = expectedOutput;
     }
 
     @Test
-    public void constructorTest() {
+    public void simpleRegexTest() {
         if (expectedException == null) {
             SimpleRegex regex = new SimpleRegex(expr, caseSensitive);
+            // Test constructor
             Assert.assertNotNull(regex);
             Assert.assertTrue(regex.matches(expr));
+
+            // Test match
+            boolean actualOutput = regex.matches(target);
+            Assert.assertEquals(expectedOutput, actualOutput);
         } else {
-            Assert.assertThrows(expectedException, () -> new SimpleRegex(expr, caseSensitive));
+            Assert.assertThrows(expectedException, () -> {
+                SimpleRegex regex = new SimpleRegex(expr, caseSensitive);
+                // Test match
+                boolean actualOutput = regex.matches(target);
+                Assert.assertEquals(expectedOutput, actualOutput);
+            });
         }
     }
 
